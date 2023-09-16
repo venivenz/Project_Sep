6d850d0d2d12a110c1fa3bc2fbf17b39903eee5b
venivenz <stkrishnaveni99@gmail.com>
2023-09-16 00:36:42 -0700

    first commit

---
+++ src/test/java/org/test/MainTest.java
@@ -0,0 +1,20 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+	
+		
+		@Test
+		
+		private void test() {
+			
+		//	write the code logic here
+			
+			
+		}
+
+	}
+
+	
+
